3bc4294fd462421bcd0ec93a35d793c9856cb77d
jcwinter-git <jcwinter@gmail.com>
2020-06-28 16:18:52 -0700

    Uploading files from local disk to GitHub

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cto_to_word
 This workflow tool is designed to take as an input a survey file in xlsx, the 
-standard for use with SurveyCTO and Open Data Kit. The script
+standard for use with SurveyCTO and Open Data Kit (ODK). The script
 outputs a plaintext version of the survey that pulls in all choices in a 
 human readable format. This is designed to address the problem where 
 researchers code surveys in a format to optimize machine readability, but 
@@ -9,3 +9,7 @@ collaborators less familiar with this format are not able to provide feedback.
 This tool is currently in beta - the output to plaintext works. The next steps
 are to render this using RMarkdown and saving it as a .docx document that can 
 then be reviewed using Track Changes. 
+
+The test file is CTO_to_word_test.xlsx. This is written in the standard ODK
+format. cto_cleaning.R is the script which translates the XLSX file into 
+plaintext. 
---
+++ cto_cleaning.R
@@ -0,0 +1,43 @@
+# Taking ODK Excel file and outputting in a table
+
+library(readxl)
+library(dplyr)
+
+# Data loading
+
+here::here()
+s = read_excel("CTO_to_word_test.xlsx", sheet = "reference")
+c = read_excel("CTO_to_word_test.xlsx", sheet = "choices")
+s = s %>% select(type, name, ID, label, hint, constraint, relevance) %>%
+  filter(is.na(label) == F) %>%
+  mutate(label = paste(ID, label, sep=" ")) %>%
+  select(-ID)
+c = c %>% select(list_name, name, label)
+output = ""
+for (i in 1:nrow(s)){
+  output = paste("", output, cto_to_word(i), sep = "\n")
+}
+cat(output)
+
+
+cto_to_word = function(index) {
+  choice_lookup = strsplit(s$type[index]," ")[[1]][2]
+  choice_list = list()
+  indices = which(c$list_name %in% choice_lookup)
+  p = list(c$label[indices])
+  attach(s)
+  text = paste(paste(label[index], name[index], sep = " - "), ifelse(is.na(hint[index])==T,"",hint[index]),
+               paste(ifelse(is.na(constraint[index]==T),"",constraint[index]),
+                     ifelse(is.na(relevance[index])==T,"",relevance[index]),
+                     sep = " "), sep = "\n")
+  detach(s)
+  for (j in 1:length(p[[1]])){
+    choice_list = paste(choice_list, p[[1]][j], sep = "\n")
+  }
+  text = paste(text, choice_list, "", sep = "\n")
+  # return(cat(text))
+  # trying to add this to a massive list, just returning text
+  return(text)
+}
+
+
